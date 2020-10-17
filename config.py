c5eee47a3a06d5d30e558d46801175ec7c58757b
肖尚华 <hallazie@gmail.com>
2020-10-17 14:17:55 +0800

    init commit

---
+++ config.py
@@ -0,0 +1,11 @@
+# --*-- coding:utf-8 --*--
+# @author: Xiao Shanghua
+# @contact: hallazie@outlook.com
+# @file: config.py
+# @time: 2020/10/17 14:16
+# @desc:
+
+OPENPOSE_ROOT_PATH = 'F:\\vision\\openpose-master\\openpose-master\\openpose-master\\build'
+
+
+
---
+++ example/openpose_skeleton.py
@@ -0,0 +1,66 @@
+# --*-- coding:utf-8 --*--
+# @author: Xiao Shanghua
+# @contact: hallazie@outlook.com
+# @file: openpose_skeleton.py
+# @time: 2020/10/17 14:09
+# @desc:
+
+import sys
+import cv2
+import os
+import argparse
+import config
+
+try:
+    root_path = config.OPENPOSE_ROOT_PATH
+    try:
+        sys.path.append(os.path.join(root_path, 'python\\openpose\\Release'))
+        os.environ['PATH'] = os.environ['PATH'] + ';' + root_path + '\\x64\\Release;' + root_path + '\\bin;'
+        import pyopenpose as op
+    except ImportError as e:
+        print('Error: OpenPose library could not be found. Did you enable `BUILD_PYTHON` in CMake and have this Python script in the right folder?')
+        raise e
+
+    # Flags
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--image_path", default="../../../examples/media/COCO_val2014_000000000192.jpg", help="Process an image. Read all standard formats (jpg, png, bmp, etc.).")
+    args = parser.parse_known_args()
+
+    # Custom Params (refer to include/openpose/flags.hpp for more parameters)
+    params = dict()
+    params["model_folder"] = "../../../models/"
+
+    # Add others in path?
+    for i in range(0, len(args[1])):
+        curr_item = args[1][i]
+        if i != len(args[1])-1: next_item = args[1][i+1]
+        else: next_item = "1"
+        if "--" in curr_item and "--" in next_item:
+            key = curr_item.replace('-','')
+            if key not in params:  params[key] = "1"
+        elif "--" in curr_item and "--" not in next_item:
+            key = curr_item.replace('-','')
+            if key not in params: params[key] = next_item
+
+    # Construct it from system arguments
+    # op.init_argv(args[1])
+    # oppython = op.OpenposePython()
+
+    # Starting OpenPose
+    opWrapper = op.WrapperPython()
+    opWrapper.configure(params)
+    opWrapper.start()
+
+    # Process Image
+    datum = op.Datum()
+    imageToProcess = cv2.imread(args[0].image_path)
+    datum.cvInputData = imageToProcess
+    opWrapper.emplaceAndPop([datum])
+
+    # Display Image
+    print("Body keypoints: \n" + str(datum.poseKeypoints))
+    cv2.imshow("OpenPose 1.6.0 - Tutorial Python API", datum.cvOutputData)
+    cv2.waitKey(0)
+except Exception as e:
+    print(e)
+    sys.exit(-1)
